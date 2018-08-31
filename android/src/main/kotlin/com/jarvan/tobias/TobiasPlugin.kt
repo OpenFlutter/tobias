be1825378a5ce9578749ec55ee7f85ac23f9593f
JarvanMo <jarvan.mo@gmail.com>
2018-08-31 23:54:57 +0800

    android api done

--- android/src/main/kotlin/com/jarvan/tobias/TobiasPlugin.kt
+++ android/src/main/kotlin/com/jarvan/tobias/TobiasPlugin.kt
@@ -1,28 +1,66 @@
 package com.jarvan.tobias
 
-import com.alipay.sdk.app.PayTask
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import com.alipay.sdk.app.PayTask
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 
-class TobiasPlugin(): MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "com.jarvanmo/tobias")
-      channel.setMethodCallHandler(TobiasPlugin())
+
+class TobiasPlugin(private var registrar: Registrar) : MethodCallHandler {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar): Unit {
+            val channel = MethodChannel(registrar.messenger(), "com.jarvanmo/tobias")
+            channel.setMethodCallHandler(TobiasPlugin(registrar))
+        }
     }
-  }
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onMethodCall(call: MethodCall, result: Result): Unit {
+        when {
+            call.method == "version" -> version(call, result)
+            call.method == "pay" -> pay(call, result)
+            else -> result.notImplemented()
+        }
+
     }
 
+    private fun pay(call: MethodCall, result: Result) {
 
-  }
+        launch(UI) {
+            val payResult = doPayTask(call.arguments as String)
+            result.success( payResult.plus( "platform" to "android"))
+        }
+    }
+
+    private suspend fun doPayTask(orderInfo: String): Map<String, String> {
+
+        return async(CommonPool) {
+            val alipay = PayTask(registrar.activity())
+            alipay.payV2(orderInfo, true) ?: mapOf<String, String>()
+        }.await()
+    }
+
+    private fun version(call: MethodCall, result: Result) {
+        launch(UI) {
+            val version = doGetVersionTask()
+            result.success(mapOf(
+                    "platform" to "android",
+                    "version" to version
+            ))
+        }
+    }
+
+    private suspend fun doGetVersionTask(): String {
+
+        return async(CommonPool) {
+            val alipay = PayTask(registrar.activity())
+            alipay.version ?: "-1.0"
+        }.await()
+    }
 }

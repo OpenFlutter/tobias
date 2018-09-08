96dab64f12ca8cef78d3cdec9b3047de3cfa7701
JarvanMo <jarvan.mo@gmail.com>
2018-09-08 11:04:07 +0800

    reformat code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
-
-import 'package:tobias/tobias.dart' as tobias;
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:tobias/tobias.dart' as tobias;
 
 void main() => runApp(new MyApp());
 

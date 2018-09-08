9a1febe30d08b0c0bccd5a73188fefc411926c2a
JarvanMo <jarvan.mo@gmail.com>
2018-09-08 11:03:41 +0800

    reformat code

--- lib/src/tobias_iml.dart
+++ lib/src/tobias_iml.dart
@@ -2,9 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-
 final MethodChannel _channel = const MethodChannel('com.jarvanmo/tobias');
-
 
 Future<Map> pay(String order) async {
   return await _channel.invokeMethod("pay", order);

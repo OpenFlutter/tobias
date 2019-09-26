7b5d77b7ef9e7b54c51aa975857718af88fd822d
JarvanMo <jarvan.mo@gmail.com>
2019-09-26 20:01:16 +0800

    del tobias_iml

--- lib/tobias.dart
+++ lib/tobias.dart
@@ -1,3 +1,25 @@
-library tobias;
+import 'dart:async';
 
-export 'src/tobias_iml.dart';
+import 'package:flutter/services.dart';
+
+final MethodChannel _channel = const MethodChannel('com.jarvanmo/tobias');
+
+/// [evn] only supports Android due to native AliPaySDK
+Future<Map> pay(String order, {AliPayEvn evn = AliPayEvn.ONLINE}) async {
+  return await _channel
+      .invokeMethod("pay", {"order": order, "payEnv": evn.index});
+}
+
+Future<Map> auth(String auth) async {
+  return await _channel.invokeMethod("auth", auth);
+}
+
+Future<String> version() async {
+  return await _channel.invokeMethod("version");
+}
+
+Future<bool> isAliPayInstalled() async {
+  return await _channel.invokeMethod("isAliPayInstalled");
+}
+
+enum AliPayEvn { ONLINE, SANDBOX }

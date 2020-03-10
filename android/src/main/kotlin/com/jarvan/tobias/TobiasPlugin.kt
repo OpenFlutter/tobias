476d3be784e38e98cc924d6f0f83c2bb4b3dca6a
JarvanMo <jarvan.mo@gmail.com>
2020-03-10 19:05:23 +0800

    fix #40

--- android/src/main/kotlin/com/jarvan/tobias/TobiasPlugin.kt
+++ android/src/main/kotlin/com/jarvan/tobias/TobiasPlugin.kt
@@ -1,5 +1,6 @@
 package com.jarvan.tobias
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -7,6 +8,8 @@ import com.alipay.sdk.app.AuthTask
 import com.alipay.sdk.app.EnvUtils
 import com.alipay.sdk.app.PayTask
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -15,14 +18,16 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import kotlinx.coroutines.*
 
 
-class TobiasPlugin() : FlutterPlugin, MethodCallHandler {
-    constructor(private var registrar: Registrar)
+class TobiasPlugin() : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private var activity: Activity? = null
 
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar): Unit {
             val channel = MethodChannel(registrar.messenger(), "com.jarvanmo/tobias")
-            channel.setMethodCallHandler(TobiasPlugin(registrar))
+            channel.setMethodCallHandler(TobiasPlugin().apply {
+                activity = registrar.activity()
+            })
         }
     }
 
@@ -52,7 +57,7 @@ class TobiasPlugin() : FlutterPlugin, MethodCallHandler {
     private suspend fun doPayTask(orderInfo: String): Map<String, String> {
 
         return GlobalScope.async(Dispatchers.Default, CoroutineStart.DEFAULT) {
-            val alipay = PayTask(registrar.activity())
+            val alipay = PayTask(activity)
             alipay.payV2(orderInfo, true) ?: mapOf<String, String>()
         }.await()
     }
@@ -68,7 +73,7 @@ class TobiasPlugin() : FlutterPlugin, MethodCallHandler {
     private suspend fun doAuthTask(authInfo: String): Map<String, String> {
 
         return GlobalScope.async(Dispatchers.Default, CoroutineStart.DEFAULT) {
-            val alipay = AuthTask(registrar.activity())
+            val alipay = AuthTask(activity)
             alipay.authV2(authInfo, true) ?: mapOf<String, String>()
         }.await()
     }
@@ -82,26 +87,43 @@ class TobiasPlugin() : FlutterPlugin, MethodCallHandler {
     }
 
     private fun isAliPayInstalled(result: Result) {
-        val manager = registrar.context().packageManager
-        val action = Intent(Intent.ACTION_VIEW)
-        action.data = Uri.parse("alipays://")
-        val list = manager.queryIntentActivities(action, PackageManager.GET_RESOLVED_FILTER)
-        result.success(list != null && list.size > 0)
+        val manager = activity?.packageManager
+        if (manager != null) {
+            val action = Intent(Intent.ACTION_VIEW)
+            action.data = Uri.parse("alipays://")
+            val list = manager.queryIntentActivities(action, PackageManager.GET_RESOLVED_FILTER)
+            result.success(list != null && list.size > 0)
+        } else {
+            result.error("-1", "can't find packageManager", null)
+        }
     }
 
     private suspend fun doGetVersionTask(): String {
 
         return GlobalScope.async(Dispatchers.Default, CoroutineStart.DEFAULT) {
-            val alipay = PayTask(registrar.activity())
+            val alipay = PayTask(activity)
             alipay.version ?: ""
         }.await()
     }
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        val channel = MethodChannel(registrar.messenger(), "com.jarvanmo/tobias")
+        val channel = MethodChannel(binding.binaryMessenger, "com.jarvanmo/tobias")
         channel.setMethodCallHandler(this)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    }
+
+    override fun onDetachedFromActivity() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
     }
 }

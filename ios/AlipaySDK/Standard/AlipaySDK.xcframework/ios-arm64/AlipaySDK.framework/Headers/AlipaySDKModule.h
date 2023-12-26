df1a5c0c429bab91cd22dc3411230bef81565728
JarvanMo <jarvan.mo@gmail.com>
2023-12-26 21:34:14 +0800

    AliPay SDK => 15.8.17

---
+++ ios/AlipaySDK/Standard/AlipaySDK.xcframework/ios-arm64/AlipaySDK.framework/Headers/AlipaySDKModule.h
@@ -0,0 +1,17 @@
+//
+//  AlipaySDKModule.h
+//  AlipaySDK
+//
+//  Created by mingsheng on 2023/11/14.
+//  Copyright © 2023 Alipay. All rights reserved.
+//
+
+#ifndef AlipaySDKModule_h
+#define AlipaySDKModule_h
+
+#import "AlipaySDK.h"
+#import "APayAuthInfo.h"
+#import "AFServiceCenter.h"
+#import "AFServiceResponse.h"
+
+#endif /* AlipaySDKModule_h */

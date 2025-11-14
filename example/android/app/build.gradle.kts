05fb74033b6945e24a3b55c15ce3638a7f95ee5a
Jarvanmo <jarvan.mo@gmail.com>
2025-11-14 13:56:59 +0800

    kotlin => 2.2.20 Jvm target => 17 AGP => 8.11.1

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -11,20 +11,20 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "co.dash.dash_living_upload_photo_example"
+        applicationId = "com.jarvan.tobias_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 27
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
